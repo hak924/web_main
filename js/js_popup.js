ae8246b8c79735385bb62cc548c3312f084e2918
admin <sdfsdf@sdf.com>
2025-04-11 16:16:18 +0900

    5,6,7주차 깃허브

---
+++ js/js_popup.js
@@ -0,0 +1,9 @@
+function pop_up() {
+    window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+    }
+    function over(obj) {
+        obj.src="image/logo.svg";
+        }
+        function out(obj) {
+        obj.src="image/logo_2.png";
+        }
